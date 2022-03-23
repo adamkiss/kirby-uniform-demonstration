7f59b62b2194444ed2c03013196bdf7d7e42972e
Adam Kiss <iam@adamkiss.com>
2022-03-23 15:48:57 +0100

    Fix comments left over from original SessionAction

--- site/plugins/uniform-actions/FirstCustomAction.php
+++ site/plugins/uniform-actions/FirstCustomAction.php
@@ -11,8 +11,11 @@ class FirstCustomAction extends Action {
             // get the name of the session variable
             $name = $this->option('name', 'session-store');
 
-            // put form into session
+            // put a custom value into session
             Kirby::instance()->session()->set($name, date('H:i:s'));
+
+            // also return the value - if you want to capture it
+            // back in controller for further modification
             return date('H:i:s');
         } catch (\Exception $e) {
             $this->fail($e->getMessage());
